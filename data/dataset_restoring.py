d54e6472a356d39ce262937a222605126b2736b2
Peter <peticigan@gmail.com>
2025-12-14 20:17:51 +0100

    novy dataset #1
    odstranenie nepotrebnych stlpcov (88:210) ktore niekto vypocital predomnou

--- data/dataset_restoring.py
+++ data/dataset_restoring.py
@@ -1,6 +1,15 @@
-import os
-import numpy as np
 import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ORIGIN_DATASET_PATH = os.getenv("ORIGIN_DATASET_PATH")
+NEW_DATASET_PATH = os.getenv("NEW_DATASET_PATH")
+
 
 if __name__ == "__main__":
-    path: str = "../datasets/All_data_preprocessed.xlsx"
+    df = pd.read_excel(ORIGIN_DATASET_PATH)
+    drop_colums: list[str] = list(df.columns[88:210])
+    df = df.drop(drop_colums, axis=1)
+
+    df.to_excel(NEW_DATASET_PATH)
